34443bf4488901f0715de97e9f4268da6092719d
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-10-31 14:52:38 +0100

    Download and export symbol statistics
    Download and export the basic statistics of all possible symbols in a single json file

--- classes/BlockchainChecker.py
+++ classes/BlockchainChecker.py
@@ -150,7 +150,79 @@ class BlockchainChecker:
 
         unique_symbol_values = [symbol["symbol"] for symbol in response_json]
 
-        return unique_symbol_values
+        all_symbols_statistics = dict()
+
+        for symbol in unique_symbol_values:
+
+            response = requests.get(self.l3_endpoint + symbol)
+            response_json = response.json()
+
+            symbol_name = response_json["symbol"]
+
+            bids = pd.json_normalize(data=response_json, record_path=["bids"])
+            asks = pd.json_normalize(data=response_json, record_path=["asks"])
+
+            bids_dict = dict
+            asks_dict = dict
+
+            if not bids.empty:
+
+                bids["value"] = bids["px"] * bids["qty"]
+
+                bids_count = bids.shape[0]
+                bids_qty_sum = np.sum(bids["qty"])
+                bids_value_sum = np.sum(bids["value"])
+
+                bids_dict = {
+                    "count": bids_count,
+                    "qty": bids_qty_sum,
+                    "value": bids_value_sum,
+                }
+
+            else:
+
+                bids_dict = {
+                    "count": [],
+                    "qty": [],
+                    "value": [],
+                }
+
+            if not asks.empty:
+
+                asks["value"] = asks["px"] * asks["qty"]
+
+                asks_count = asks.shape[0]
+                asks_qty_sum = np.sum(asks["qty"])
+                asks_value_sum = np.sum(asks["value"])
+
+                asks_dict = {
+                    "count": asks_count,
+                    "qty": asks_qty_sum,
+                    "value": asks_value_sum,
+                }
+
+            else:
+
+                asks_dict = {
+                    "count": [],
+                    "qty": [],
+                    "value": [],
+                }
+
+            bids_asks_dict = {"bids": bids_dict, "asks": asks_dict}
+            all_symbols_statistics[symbol_name] = bids_asks_dict
+
+        all_symbols_statistics_json = json.dumps(all_symbols_statistics)
+
+        all_folder_path = "data/all"
+        date = str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))
+
+        with open(
+            f"{all_folder_path}/all_statistics_{date}.json", "w"
+        ) as all_statistics_file:
+            all_statistics_file.write(all_symbols_statistics_json)
+
+        return "\n[OK] Statistics for all symbols have been downloaded succesfully"
 
     def exit_app(self) -> str:
         self.app_on = False
