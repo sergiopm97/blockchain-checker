5168040f71282e74a619e5b363e0a865ccf3db66
Sergio Peña <sergio@MacBook-Pro-de-Sergio.local>
2022-11-02 16:51:55 +0100

    Fix bug get_symbol_data
    Fixed bug in the get_symbol_data function in which an error occurred when the requested symbol returned empty fields

--- classes/BlockchainChecker.py
+++ classes/BlockchainChecker.py
@@ -38,97 +38,124 @@ class BlockchainChecker:
             asks = pd.json_normalize(data=json_response, record_path=["asks"])
             date = str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))
 
-            bids["value"] = bids["px"] * bids["qty"]
+            bids_statistics = dict()
+            asks_statistics = dict()
 
-            bids_average_value = float(np.mean(bids["value"]))
-
-            bid_max_value = bids[bids["value"] == np.max(bids["value"])]
-
-            bids_greater_px = float(bid_max_value["px"])
-            bids_greater_qty = float(bid_max_value["qty"])
-            bids_greater_num = float(bid_max_value["num"])
-            bids_greater_value = float(bid_max_value["value"])
-
-            bids_greater_value_dict = {
-                "px": bids_greater_px,
-                "qty": bids_greater_qty,
-                "num": bids_greater_num,
-                "value": bids_greater_value,
-            }
-
-            bid_min_value = bids[bids["value"] == np.min(bids["value"])]
-
-            bids_lesser_px = float(bid_min_value["px"])
-            bids_lesser_qty = float(bid_min_value["qty"])
-            bids_lesser_num = float(bid_min_value["num"])
-            bids_lesser_value = float(bid_min_value["value"])
-
-            bids_lesser_value_dict = {
-                "px": bids_lesser_px,
-                "qty": bids_lesser_qty,
-                "num": bids_lesser_num,
-                "value": bids_lesser_value,
-            }
-
-            bids_total_qty = float(np.sum(bids["qty"]))
-            bids_total_px = int(np.sum(bids["px"]))
-
-            bids_statistics = {
-                "bids": {
-                    "average_value": bids_average_value,
-                    "greater_value": bids_greater_value_dict,
-                    "lesser_value": bids_lesser_value_dict,
-                    "total_qty": bids_total_qty,
-                    "total_px": bids_total_px,
+            if bids.empty:
+                bids_statistics = {
+                    "bids": {
+                        "average_value": [],
+                        "greater_value": [],
+                        "lesser_value": [],
+                        "total_qty": [],
+                        "total_px": [],
+                    }
                 }
-            }
+
+            else:
+                bids["value"] = bids["px"] * bids["qty"]
+
+                bids_average_value = float(np.mean(bids["value"]))
+
+                bid_max_value = bids[bids["value"] == np.max(bids["value"])]
+
+                bids_greater_px = float(bid_max_value["px"])
+                bids_greater_qty = float(bid_max_value["qty"])
+                bids_greater_num = float(bid_max_value["num"])
+                bids_greater_value = float(bid_max_value["value"])
+
+                bids_greater_value_dict = {
+                    "px": bids_greater_px,
+                    "qty": bids_greater_qty,
+                    "num": bids_greater_num,
+                    "value": bids_greater_value,
+                }
+
+                bid_min_value = bids[bids["value"] == np.min(bids["value"])]
+
+                bids_lesser_px = float(bid_min_value["px"])
+                bids_lesser_qty = float(bid_min_value["qty"])
+                bids_lesser_num = float(bid_min_value["num"])
+                bids_lesser_value = float(bid_min_value["value"])
+
+                bids_lesser_value_dict = {
+                    "px": bids_lesser_px,
+                    "qty": bids_lesser_qty,
+                    "num": bids_lesser_num,
+                    "value": bids_lesser_value,
+                }
+
+                bids_total_qty = float(np.sum(bids["qty"]))
+                bids_total_px = int(np.sum(bids["px"]))
+
+                bids_statistics = {
+                    "bids": {
+                        "average_value": bids_average_value,
+                        "greater_value": bids_greater_value_dict,
+                        "lesser_value": bids_lesser_value_dict,
+                        "total_qty": bids_total_qty,
+                        "total_px": bids_total_px,
+                    }
+                }
 
             bids_statistics_json = json.dumps(bids_statistics)
 
-            asks["value"] = asks["px"] * asks["qty"]
-
-            asks_average_value = float(np.mean(asks["value"]))
-
-            ask_max_value = asks[asks["value"] == np.max(asks["value"])]
-
-            asks_greater_px = float(ask_max_value["px"])
-            asks_greater_qty = float(ask_max_value["qty"])
-            asks_greater_num = float(ask_max_value["num"])
-            asks_greater_value = float(ask_max_value["value"])
-
-            asks_greater_value_dict = {
-                "px": asks_greater_px,
-                "qty": asks_greater_qty,
-                "num": asks_greater_num,
-                "value": asks_greater_value,
-            }
-
-            ask_min_value = asks[asks["value"] == np.min(asks["value"])]
-
-            asks_lesser_px = float(ask_min_value["px"])
-            asks_lesser_qty = float(ask_min_value["qty"])
-            asks_lesser_num = float(ask_min_value["num"])
-            asks_lesser_value = float(ask_min_value["value"])
-
-            asks_lesser_value_dict = {
-                "px": asks_lesser_px,
-                "qty": asks_lesser_qty,
-                "num": asks_lesser_num,
-                "value": asks_lesser_value,
-            }
-
-            asks_total_qty = float(np.sum(asks["qty"]))
-            asks_total_px = int(np.sum(asks["px"]))
-
-            asks_statistics = {
-                "asks": {
-                    "average_value": asks_average_value,
-                    "greater_value": asks_greater_value_dict,
-                    "lesser_value": asks_lesser_value_dict,
-                    "total_qty": asks_total_qty,
-                    "total_px": asks_total_px,
+            if asks.empty:
+                asks_statistics = {
+                    "asks": {
+                        "average_value": [],
+                        "greater_value": [],
+                        "lesser_value": [],
+                        "total_qty": [],
+                        "total_px": [],
+                    }
                 }
-            }
+
+            else:
+                asks["value"] = asks["px"] * asks["qty"]
+
+                asks_average_value = float(np.mean(asks["value"]))
+
+                ask_max_value = asks[asks["value"] == np.max(asks["value"])]
+
+                asks_greater_px = float(ask_max_value["px"])
+                asks_greater_qty = float(ask_max_value["qty"])
+                asks_greater_num = float(ask_max_value["num"])
+                asks_greater_value = float(ask_max_value["value"])
+
+                asks_greater_value_dict = {
+                    "px": asks_greater_px,
+                    "qty": asks_greater_qty,
+                    "num": asks_greater_num,
+                    "value": asks_greater_value,
+                }
+
+                ask_min_value = asks[asks["value"] == np.min(asks["value"])]
+
+                asks_lesser_px = float(ask_min_value["px"])
+                asks_lesser_qty = float(ask_min_value["qty"])
+                asks_lesser_num = float(ask_min_value["num"])
+                asks_lesser_value = float(ask_min_value["value"])
+
+                asks_lesser_value_dict = {
+                    "px": asks_lesser_px,
+                    "qty": asks_lesser_qty,
+                    "num": asks_lesser_num,
+                    "value": asks_lesser_value,
+                }
+
+                asks_total_qty = float(np.sum(asks["qty"]))
+                asks_total_px = int(np.sum(asks["px"]))
+
+                asks_statistics = {
+                    "asks": {
+                        "average_value": asks_average_value,
+                        "greater_value": asks_greater_value_dict,
+                        "lesser_value": asks_lesser_value_dict,
+                        "total_qty": asks_total_qty,
+                        "total_px": asks_total_px,
+                    }
+                }
 
             asks_statistics_json = json.dumps(asks_statistics)
 
